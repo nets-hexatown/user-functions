b96c2849c22cbf02134997bf308043d54640dc70
niels <niels@timeboxer.dk>
2017-04-10 11:37:26 +0200

    added a lot

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# user-functions
-"Click and play" functions directly hostable in e.g. Azure Functions
+# User Management and adoption tracking
+
+### Summary ###
+This solution contains a library of "Click and play" functions that allows you to perform complex provisioning and artifact management actions towards Offic365 and other cloud services. The commands use CSOM and can work against both SharePoint Online as SharePoint On-Premises.
+
+![Hexatown](./_media/hexatown-logo-48h.png)
+
+### Applies to ###
+-  Office 365 Multi Tenant (MT)
+
+
+### Prerequisites ###
+- Microsoft Azure subscription
+- Microsoft Office 365 Tenant
+
+### Disclaimer ###
+**THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.**
+
+### Credits ###
+(https://github.com/SharePoint/PnP-PowerShell)
---
+++ useradoption-updatestatus/run.ps1
@@ -0,0 +1,57 @@
+Import-Module "..\_modules\hexa-functions.psm1"
+Import-Module "..\_modules\SharePointPnPPowerShellOnline\2.14.1704.0\SharePointPnPPowerShellOnline.psd1"
+
+
+Enter-Hexa $req $res $PSScriptRoot
+
+$tenant = $env:O365TENANT
+$relativeUrl = $global:request.siteRelativeUrl
+$url = "https://$tenant.sharepoint.com$relativeUrl"
+Connect-PnPOnline -Url $url -Credentials ($global:credentials)
+
+$userAdoptionListname = "User Adoption Status"
+
+function CreateUserAdoptionList(){
+    # Remove-PnPList -Identity $listname -Force
+    write-host "Checking status list"
+    $list = Get-PnPList $listname 
+    if ($list -eq $null){
+
+        Write-Output "Creating list $userAdoptionListname"
+        New-PnPList -Title $userAdoptionListname -Template "Custom"
+        Set-PnPList -Identity $userAdoptionListname -EnableVersioning $true
+        Add-PnPField -List $userAdoptionListname -DisplayName "User Email" -InternalName "user_email" -Type:Text -AddToDefaultView
+        Add-PnPField -List $userAdoptionListname -DisplayName "Location" -InternalName "location" -Type:Text  -AddToDefaultView
+        Add-PnPField -List $userAdoptionListname -DisplayName "Country" -Type:Text -InternalName "country" -AddToDefaultView
+        Add-PnPField -List $userAdoptionListname -DisplayName "Department ID" -InternalName "departmentid" -Type:Text -AddToDefaultView
+        Add-PnPField -List $userAdoptionListname -DisplayName "L1" -InternalName "l1" -Type:Text  -AddToDefaultView
+        Add-PnPField -List $userAdoptionListname -DisplayName "L2" -InternalName "l2" -Type:Text 
+        Add-PnPField -List $userAdoptionListname -DisplayName "L3" -InternalName "l3" -Type:Text 
+        Add-PnPField -List $userAdoptionListname -DisplayName "L4" -InternalName "l4" -Type:Text 
+        Add-PnPField -List $userAdoptionListname -DisplayName "L5" -InternalName "l5" -Type:Text 
+
+        # Add-PnPView -List $listname -Title "Overview" -Fields "Title","User Email" -SetAsDefault:$true -ViewType:Html
+
+       
+     }
+}
+
+CreateUserAdoptionList
+
+Write-Output "Adding test entry"
+$list = Get-PnPList $userAdoptionListname 
+$itemcreateinfo = New-Object Microsoft.SharePoint.Client.ListItemCreationInformation
+$listitem = $list.AddItem($itemcreateinfo)
+$listitem["Title"] = "Niels (niels@365admin.net)"
+$listitem["user_email"] = "niels@365admin.net"
+$listitem.Update()
+Execute-PnPQuery
+
+
+
+
+
+
+Exit-Hexa $result
+
+
